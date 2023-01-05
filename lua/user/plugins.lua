33607df23fb1806abe792fa6e6dd76327996a197
NIkita <nikitosgroover@gmail.com>
2023-01-06 03:40:09 +0600

    Update handlers

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,6 +65,7 @@ return packer.startup(function(use)
   use "williamboman/mason.nvim"
   use "williamboman/mason-lspconfig.nvim"
   use "jose-elias-alvarez/null-ls.nvim"
+  use "RRethy/vim-illuminate"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
