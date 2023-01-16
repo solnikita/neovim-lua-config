72c65c3936be8a1d95befcd7a63108449b7db96f
NIkita <nikitosgroover@gmail.com>
2023-01-17 01:42:49 +0600

    Telescope config

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -8,9 +8,36 @@ local actions = require "telescope.actions"
 telescope.setup {
   defaults = {
 
-    prompt_prefix = " ",
-    selection_caret = " ",
     path_display = { "smart" },
+    prompt_prefix = "  ",
+    selection_caret = "  ",
+    entry_prefix = "  ",
+    winblend = 0,
+    border = {},
+    color_devicons = true,
+    use_less = true,
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    set_env = { ["COLORTERM"] = "truecolor" },
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
 
     mappings = {
       i = {
