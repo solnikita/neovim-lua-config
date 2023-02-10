6bd5706b99e29c07d5a6eca1f28b7d258fd47ea2
NIkita <nikitosgroover@gmail.com>
2023-02-11 02:40:09 +0600

    dracula for which key

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,14 @@
 local colorscheme = "dracula"
+local colors = require("user.colors.dracula")
+
+-- Applying colors
+local function set_colors(fg, bg)
+	return "guifg=" .. fg .. " guibg=" .. bg
+end
+local api = vim.api
+api.nvim_command("hi WhichKeyGroup " .. set_colors(colors.purple, colors.bg))
+api.nvim_command("hi WhichKeyFloat " .. set_colors(colors.orange, colors.bg))
+api.nvim_command("hi WhichKeyBorder " .. set_colors(colors.bg, colors.bg))
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
