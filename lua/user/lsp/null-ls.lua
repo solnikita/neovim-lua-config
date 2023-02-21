88c510fe34e70534d074c0a0094222d1647c6716
NIkita <nikitosgroover@gmail.com>
2023-02-21 18:40:12 +0600

    add prettierd

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -6,6 +6,7 @@ end
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.eslint_d,
     },
 })
