9347a6cd1b88f410bf4aec3a23b7d0f597554cc6
NIkita <nikitosgroover@gmail.com>
2023-03-30 13:12:17 +0600

    update packages and fix buferline separator style for transparent

--- lua/plugins/configs/bufferline.lua
+++ lua/plugins/configs/bufferline.lua
@@ -61,8 +61,8 @@ bufferline.setup({
 		--   end
 		-- end,
 		offsets = {
-			{ filetype = "vista", text = "Tags", padding = 1 },
-			{ filetype = "nvim-tree", text = "Explorer", padding = 1 },
+			{ filetype = "vista", text = "Tags", padding = 0 },
+			{ filetype = "nvim-tree", text = "Explorer", padding = 0 },
 		},
 		show_buffer_icons = true,
 		show_buffer_close_icons = true,
@@ -71,7 +71,7 @@ bufferline.setup({
 		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 		-- can also be a table containing 2 custom separators
 		-- [focused and unfocused]. eg: { '|', '|' }
-		separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+		separator_style = {"", ""}, -- | "thick" | "thin" | { 'any', 'any' },
 		enforce_regular_tabs = true,
 		always_show_bufferline = true,
 		-- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
